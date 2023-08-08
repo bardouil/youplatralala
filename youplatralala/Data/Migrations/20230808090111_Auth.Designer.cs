9da4db02a4ef42d8c3d0cf06e753ca604325195f
pc <pc@DESKTOP-DKTBBHV>
2023-08-08 11:15:59 +0200

    Authentification

--- youplatralala/Data/Migrations/20230808090111_Auth.Designer.cs
+++ youplatralala/Data/Migrations/20230808090111_Auth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using youplatralala.Data;
 
@@ -11,9 +12,10 @@ using youplatralala.Data;
 namespace youplatralala.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230808090111_Auth")]
+    partial class Auth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ youplatralala/ReadMe.md
@@ -0,0 +1,2 @@
+Migration: Update-Database depuis le gestionnaire de package 
+Installation d'une base SQL Server en local (localhost)
